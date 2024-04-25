c43f3d7ed43994077e0d6789f52244a16a997bc6
Ermiyas <ermiyasabiye1999@gmail.com>
2024-04-25 15:46:23 +0300

    pascals triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,17 @@
+def pascal_triangle(n):
+    arr = []
+    for i in range(n):
+        arr_i = []
+        arr_i.append(1)
+        if i == 1:
+            arr_i.append(1)
+            arr.append(arr_i)
+            continue
+        if i == 0:
+            arr.append(arr_i)
+            continue
+        for j in range(1, i):
+            arr_i.append(arr[i - 1][j - 1] + arr[i - 1][j])
+        arr_i.append(1)
+        arr.append(arr_i)
+    return arr
