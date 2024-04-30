3dba75bdb7b080568ad6cbdf715cfa132499e50d
Ermiyas <ermiyasabiye1999@gmail.com>
2024-04-30 21:01:07 +0300

    locked boxes documented

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,12 +1,23 @@
 #!/usr/bin/python3
+"""
+module for lockboxes
+"""
+
 
 def solveRecursively(originalBox, openedBox, openedBoxes):
+    """
+    Recursive function to solve the boxes
+    """
     notOpened = set(openedBox) - openedBoxes
     for key in notOpened:
         openedBoxes.add(key)
         solveRecursively(originalBox, originalBox[key], openedBoxes)
-    
+
+
 def canUnlockAll(boxes):
+    """
+    Method to determine if all boxes can be opened
+    """
     openedBoxes = set([0])
     for key in boxes[0]:
         if key not in openedBoxes:
