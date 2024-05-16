20c3407ed0d8319aaea5696e30022bde1b4d16a9
Ermiyas <ermiyasabiye1999@gmail.com>
2024-05-16 17:44:11 +0300

    log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """module for parsing log."""
 import sys
+import re
 
 if __name__ == "__main__":
     file_size = 0
@@ -14,26 +15,30 @@ if __name__ == "__main__":
             "405": 0,
             "500": 0
     }
+    regex = re.compile(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+\] "GET /projects/260 HTTP/1.1" (.{3}) (\d+)') # nopep8
     i = 0
     try:
         for line in sys.stdin:
-            splited = line[: -1].split(" ")
-            if len(splited) != 9:
-                continue
-            if splited[-2] not in status_codes.keys():
-                continue
-            if not splited[-1].isdigit():
-                continue
-            status_codes[splited[-2]] += 1
-            file_size += int(splited[-1])
-            i += 1
+            line = line.strip()
+            match = regex.fullmatch(line)
+            if match:
+                i += 1
+                splited = line[: -1].split(" ")
+                if len(splited) != 9:
+                    continue
+                if splited[-2] not in status_codes.keys():
+                    continue
+                if not splited[-1].isdigit():
+                    continue
+                status_codes[splited[-2]] += 1
+                file_size += int(splited[-1])
 
-            if i % 10 == 0:
-                print(f'File size: {file_size}')
-                for key, value in status_codes.items():
-                    if value == 0:
-                        continue
-                    print(f'{key}: {value}')
+                if i % 10 == 0:
+                    print(f'File size: {file_size}')
+                    for key, value in status_codes.items():
+                        if value == 0:
+                            continue
+                        print(f'{key}: {value}')
     finally:
         print(f'File size: {file_size}')
         for key, value in status_codes.items():
