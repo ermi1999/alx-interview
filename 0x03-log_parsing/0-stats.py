641c161eadd5e91e6359bf4f57e3c4b1038647e7
Ermiyas <ermiyasabiye1999@gmail.com>
2024-05-16 15:32:51 +0300

    fixed printing even if the value is 0

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,43 +2,42 @@
 """module for parsing log."""
 import sys
 
-if __name__ == "__main__":
-    file_size = 0
-    status_codes = {
-            "200": 0,
-            "301": 0,
-            "400": 0,
-            "401": 0,
-            "403": 0,
-            "404": 0,
-            "405": 0,
-            "500": 0
-    }
-    i = 0
-    try:
-        for line in sys.stdin:
-            splited = line[: -1].split(" ")
-            if len(splited) != 9:
-                continue
-            if splited[-2] not in status_codes.keys():
-                print(f"status code not found: {splited[-2]}")
-                continue
-            if not splited[-1].isdigit():
-                continue
-            status_codes[splited[-2]] += 1
-            file_size += int(splited[-1])
+file_size = 0
+status_codes = {
+        "200": 0,
+        "301": 0,
+        "400": 0,
+        "401": 0,
+        "403": 0,
+        "404": 0,
+        "405": 0,
+        "500": 0
+}
+i = 0
+try:
+    for line in sys.stdin:
+        splited = line[: -1].split(" ")
+        if len(splited) != 9:
+            continue
+        if splited[-2] not in status_codes.keys():
+            print(f"status code not found: {splited[-2]}")
+            continue
+        if not splited[-1].isdigit():
+            continue
+        status_codes[splited[-2]] += 1
+        file_size += int(splited[-1])
 
-            i += 1
-            if i == 10:
-                print(f'File size: {file_size}')
-                for key, value in status_codes.items():
-                    if value == 0:
-                        continue
-                    print(f'{key}: {value}')
-                i = 0
-    except KeyboardInterrupt:
-        print(f'File size: {file_size}')
-        for key, value in status_codes.items():
-            if value == 0:
-                continue
-            print(f'{key}: {value}')
+        i += 1
+        if i == 10:
+            print(f'File size: {file_size}')
+            for key, value in status_codes.items():
+                if value == 0:
+                    continue
+                print(f'{key}: {value}')
+            i = 0
+except KeyboardInterrupt:
+    print(f'File size: {file_size}')
+    for key, value in status_codes.items():
+        if value == 0:
+            continue
+        print(f'{key}: {value}')
