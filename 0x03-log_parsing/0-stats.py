ca4eb05c43751d705744d8c518aa23935e8dc2e5
Ermiyas <ermiyasabiye1999@gmail.com>
2024-05-16 17:50:37 +0300

    log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,50 +1,42 @@
 #!/usr/bin/python3
-"""
-log parsing
-"""
-
+"""module for parsing log."""
 import sys
-import re
-
-
-def output(log: dict) -> None:
-    """
-    helper function to display stats
-    """
-    print("File size: {}".format(log["file_size"]))
-    for code in sorted(log["code_frequency"]):
-        if log["code_frequency"][code]:
-            print("{}: {}".format(code, log["code_frequency"][code]))
-
 
 if __name__ == "__main__":
-    regex = re.compile(
-    r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+\] "GET /projects/260 HTTP/1.1" (.{3}) (\d+)')  # nopep8
-
-    line_count = 0
-    log = {}
-    log["file_size"] = 0
-    log["code_frequency"] = {
-        str(code): 0 for code in [
-            200, 301, 400, 401, 403, 404, 405, 500]}
-
+    file_size = 0
+    status_codes = {
+            "200": 0,
+            "301": 0,
+            "400": 0,
+            "401": 0,
+            "403": 0,
+            "404": 0,
+            "405": 0,
+            "500": 0
+    }
+    i = 0
     try:
         for line in sys.stdin:
-            line = line.strip()
-            match = regex.fullmatch(line)
-            if (match):
-                line_count += 1
-                code = match.group(1)
-                file_size = int(match.group(2))
+            splited = line[: -1].split(" ")
+            if len(splited) != 9:
+                continue
+            if splited[-2] not in status_codes.keys():
+                continue
+            if not splited[-1].isdigit():
+                continue
+            status_codes[splited[-2]] += 1
+            file_size += int(splited[-1])
+            i += 1
 
-                # File size
-                log["file_size"] += file_size
-
-                # status code
-                if (code.isdecimal()):
-                    log["code_frequency"][code] += 1
-
-                if (line_count % 10 == 0):
-                    output(log)
+            if i % 10 == 0:
+                print(f'File size: {file_size}')
+                for key, value in status_codes.items():
+                    if value == 0:
+                        continue
+                    print(f'{key}: {value}')
     finally:
-        output(log)
+        print(f'File size: {file_size}')
+        for key, value in status_codes.items():
+            if value == 0:
+                continue
+            print(f'{key}: {value}')
