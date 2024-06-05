ff44a3c07aa27f3ba5c7b44d350bbae97686768f
Ermiyas <ermiyasabiye1999@gmail.com>
2024-06-05 12:41:36 -0700

    fixed starwars request

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,4 +1,26 @@
 #!/usr/bin/node
 const request = require('request');
 
-request()
+function Request (url) {
+  return new Promise((resolve, reject) => {
+    request.get(url, (error, res, body) => {
+      if (error) reject(error);
+      else resolve(JSON.parse(body));
+    });
+  });
+}
+
+async function starwars () {
+  if (process.argv < 3) return;
+
+  const Url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+  const movie = await Request(Url);
+
+  if (movie.characters === undefined) return;
+  for (const characterUrl of movie.characters) {
+    const character = await Request(characterUrl);
+    console.log(character.name);
+  }
+}
+
+starwars();
