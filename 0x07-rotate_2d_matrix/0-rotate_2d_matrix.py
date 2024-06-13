f372ed1c9231449d2c5d1ced81c2130d813df0d0
Ermiyas <ermiyasabiye1999@gmail.com>
2024-06-13 11:42:33 +0300

    rotate 2d matrix

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""rotate 2d matrix
+"""
+from typing import List
+
+
+def rotate_2d_matrix(matrix: List[List[int]]) -> None:
+    """rotate 2d matrix
+    """
+    left, r = 0, len(matrix) - 1
+    while left < r:
+        for i in range(r - left):
+            top, bottom = left, r
+            topLeft = matrix[top][left + i]
+
+            matrix[top][left + i] = matrix[bottom - i][left]
+            matrix[bottom - i][left] = matrix[bottom][r - i]
+            matrix[bottom][r - i] = matrix[top + i][r]
+            matrix[top + i][r] = topLeft
+        r -= 1
+        left += 1
